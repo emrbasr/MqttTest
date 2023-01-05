ed5a28963f3d3e4da525e99d4d8ca2643a135639
Emre <emrebasar134@gmail.com>
2023-01-05 18:47:05 +0300

    son hali

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -9,23 +9,85 @@ namespace Publisher
     {
         static void Main(string[] args)
         {
+
             MqttClient mqttClient = new MqttClient("localhost");
             string clientId = Guid.NewGuid().ToString();
             mqttClient.MqttMsgPublishReceived += client_recievedMessage;
             mqttClient.Connect(clientId);
             Console.WriteLine("Abone Olundu: test/deneme");
 
+            string Topic = "";
 
-            while (true)
+
+        //don:
+
+
+        //switch (secim)
+        //{
+        //    case 1:
+        //        Console.WriteLine("Mesajı girin: ");
+
+        //        string message = Console.ReadLine();
+
+
+        //        mqttClient.Publish(Topic, Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, true);
+        //        break;
+        //    case 2:
+        //        mqttClient.Subscribe(new String[] { Topic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+        //        break;
+
+        //    case 3:
+        //        goto don;
+
+        //}
+        Don:
+            Console.WriteLine("1- NodeServer");
+            Console.WriteLine("2- NodeCliend");
+            Console.WriteLine("3 - Cikis");
+            Console.WriteLine("** - Anasayfa");
+            Topic = Console.ReadLine();
+
+
+
+
+            do
             {
-                Console.WriteLine("Mesajı girin: ");
 
-                string message = Console.ReadLine();
-                string Topic = "test/deneme";
 
-                mqttClient.Publish(Topic, Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, true);
-                recievedMessage(mqttClient);
-            }
+
+
+
+
+
+
+                if (Topic == "2")
+                {
+                    mqttClient.Subscribe(new String[] { Topic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+
+                }
+
+                else if (Topic == "1")
+                {
+                    Console.WriteLine("Mesajı girin: ");
+
+                    string message = Console.ReadLine();
+
+
+                    mqttClient.Publish(Topic, Encoding.UTF8.GetBytes(message), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, true);
+
+                }
+                else if (Topic == "**")
+                {
+                    goto Don;
+                }
+
+
+
+            } while (Topic != "3");
+
+
+
+
         }
 
         static void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
@@ -33,10 +95,6 @@ namespace Publisher
             var message1 = System.Text.Encoding.Default.GetString(e.Message);
             System.Console.WriteLine("Cliend Mesaj: " + message1);
         }
-        static void recievedMessage(MqttClient mqttClient)
-        {
-            string Topic = "test/deneme";
-            mqttClient.Subscribe(new String[] { Topic }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
-        }
+
     }
 }
