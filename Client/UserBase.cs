c6277888a1d1988a1290efd0c4b798c9099956ed
Emre <emrebasar134@gmail.com>
2023-01-06 20:36:59 +0300

    en son

---
+++ Client/UserBase.cs
@@ -0,0 +1,26 @@
+﻿using uPLibrary.Networking.M2Mqtt;
+namespace Client
+{
+    public class UserBase
+    {
+
+        MqttClient qwe = new MqttClient("localhost");
+
+
+        //MqttClient mqttClient = new MqttClient("localhost");
+        //mqttClient.MqttMsgPublishReceived += client_recievedMessage;
+
+
+        //    string clientId = Guid.NewGuid().ToString();
+        //mqttClient.Connect(clientId);
+
+        //void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
+        //{
+        //    var message1 = System.Text.Encoding.Default.GetString(e.Message);
+        //    System.Console.WriteLine("Publish Mesaj:  " + message1);
+        //}
+
+    }
+
+
+}
